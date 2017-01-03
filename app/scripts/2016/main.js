4fb2abbdeef28393a5a6a8b4d96bc03092e6860c
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2017-01-03 12:40:38 +0100

    Reset coming soon page for 2017

---
+++ app/scripts/2016/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var $navLinks = $(".nav a"),
+    $nav = $(".nav");
+
+$navLinks.on("click", function(){
+    // Toggle active link in nav
+    $nav.find(".active").removeClass("active");
+    $nav.find(".active-re").removeClass("active-re");
+    $(this).parent().addClass("active active-re");
+
+    // Hide navbar toggle if visible
+    var $navbarToggle = $(".navbar-toggle");
+    if ($navbarToggle.is(":visible")) {
+        $navbarToggle.click();
+    }
+});
+
+var hash = location.hash;
+
+if (hash !== "") {
+    var $a = $("a[href='"+hash+"']");
+    $nav.find(".active").removeClass("active");
+    $nav.find(".active-re").removeClass("active-re");
+    $a.parent().addClass("active active-re");
+}
