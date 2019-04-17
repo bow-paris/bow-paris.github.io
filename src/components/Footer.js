a5b08964f2b0099e498ee022c6544f1364787927
Thomas BRISBOUT <thomas.brisbout@gmail.com>
2019-04-17 22:49:38 +0200

    Move media buttons to site header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = props => (
   <footer id="footer">
-    <ul className="actions">
+    <ul
+      className="actions"
+      style={{
+        width: '100%',
+        margin: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <li>
         <a href="http://confcodeofconduct.com/">
           <span className="label">Code de conduite</span>
@@ -22,33 +31,6 @@ const Footer = props => (
         <a href="/2016">
           <span className="label">2016</span>
         </a>
-      </li>
-    </ul>
-    <ul className="actions">
-      <li>
-        <a
-          href="https://twitter.com/bestofwebconf"
-          className="icon fa-twitter alt"
-        >
-          <span className="label">Twitter</span>
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://www.youtube.com/channel/UCqU8dtoFv2r0cA795pHvtOw/videos"
-          className="icon fa-youtube alt"
-        >
-          <span className="label">Youtube</span>
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/bow-paris" className="icon fa-github alt">
-          <span className="label">GitHub</span>
-        </a>
-      </li>
-
-      <li>
-        Design:<a href="https://html5up.net"> HTML5 UP</a>
       </li>
     </ul>
   </footer>
