e9f66ff0073a67fe559a4effd821c04e8cb83818
Brice Coquereau <brice@coquereau.fr>
2020-02-04 23:32:49 +0100

    ajout nestjs

--- src/data.js
+++ src/data.js
@@ -14,6 +14,7 @@ import techlunch from './assets/images/meetups-logos/tech-lunch.jpg'
 import duchess from './assets/images/meetups-logos/duchess-france.svg'
 import pwa from './assets/images/meetups-logos/pwa.jpg'
 import crafters from './assets/images/meetups-logos/software-crafters.jpg'
+import nestJs from './assets/images/meetups-logos/nestjs.svg'
 
 import grandeCrypte from './assets/images/partners/lagrandecrypte.png'
 import eventLama from './assets/images/partners/eventlama-white.png'
@@ -85,6 +86,9 @@ export const meetups = [
   {
     name: 'Software Crafters Paris',
     logo: crafters,
+  },{
+    name: 'NestJS Paris',
+    logo: nestJs,
   },
 ]
 
